411d2acb97505402ff854d98972f27e60656788e
Newton Nath <newtonnath07@gmail.com>
2021-01-05 03:06:43 +0000

    first commit

--- Problem Solving Assignment1.js
+++
@@ -1,7 +0,0 @@
-// 1st problem
-function feetToMile(lengthInFeet){
-    var howLong= lengthInFeet/5280;
-    return howLong;
-}
-var urLength= feetToMile(8000);
-console.log(urLength)
--- lengthTestArray.js
+++
@@ -1,2 +0,0 @@
-var names=['rakiba', 'sakkkib','jaoleel','ontor','miru', 'khairu'];
-console.log(names[2].length)
